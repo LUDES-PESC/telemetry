78307966b055053f017c549cc8851678f0f19e72
Allan David <allanbrados@gmail.com>
2019-04-20 16:46:56 -0300

    Remove unused variable positionsTracked

--- Assets/Scripts/Telemetry/PositionTracker.cs
+++ Assets/Scripts/Telemetry/PositionTracker.cs
@@ -5,29 +5,31 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
-public class PositionTracker : MonoBehaviour {
+public class PositionTracker : MonoBehaviour
+{
 
-	[SerializeField] float trackingDelay = 0f;
+    [SerializeField] float trackingDelay = 0f;
 
-	List<Vector3> positionsTracked = new List<Vector3>();
+    int lastPositionId = -1;
 
-	int lastPositionId = -1;
+    void Start()
+    {
+        InvokeRepeating("track", 0f, trackingDelay);
+    }
 
-	void Start () {
-		InvokeRepeating("track", 0f, trackingDelay);		
-	}
+    void track()
+    {
+        TelemetryNode playerPosition = new TelemetryNode(
+            TelemetryNodeType.Atomic,
+            "Player Position",
+            transform.position
+        );
 
-	void track() {
-		TelemetryNode playerPosition = new TelemetryNode(
-			TelemetryNodeType.Atomic,
-			"Player Position",
-			transform.position
-		);
+        if (lastPositionId != -1)
+        {
+            playerPosition.setLink(lastPositionId);
+        }
 
-		if(lastPositionId != -1) {
-			playerPosition.setLink(lastPositionId);
-		}
-
-		lastPositionId = TelemetryCore.addNode(playerPosition);
-	}
+        lastPositionId = TelemetryCore.addNode(playerPosition);
+    }
 }
